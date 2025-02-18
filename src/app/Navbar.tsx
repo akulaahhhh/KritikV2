9b6a25e0feb32f7e1551a46a045446b125b4da4a
jadMizan <asjadmizan@gmail.com>
2025-02-19 00:49:38 +0800

    Add theme toggle

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,11 +1,22 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { ModeToggle } from "@/components/ui/ModeToggle";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
-      <div className="max-w-6xl mx-auto flex justify-between">
-        <Link href="/" className="text-white text-lg hover:text-gray-300">Home</Link>
-        <Link href="/Scrapper" className="text-white text-lg hover:text-gray-300">Scrapper</Link>
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        <Link href="/" className="text-white text-lg hover:text-gray-300">
+          Home
+        </Link>
+        <div className="flex items-center gap-5">
+          <Link
+            href="/Scrapper"
+            className="text-white text-lg hover:text-gray-300"
+          >
+            Scrapper
+          </Link>
+          <ModeToggle></ModeToggle>
+        </div>
       </div>
     </nav>
   );
